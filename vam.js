47c963ab9fdc782b06321be4bfa3e973b1148be2
abhi-sinner <43845483+abhi-sinner@users.noreply.github.com>
2020-12-19 11:24:59 +0530

    Update vam.js

--- vam.js
+++ vam.js
@@ -20,18 +20,18 @@ puppeteer
   .then(function(html) {
    
     
-    $('.col-sm-6.lbl-licitacao', html).each(function() {
-      console.log($(this).text());
-    });
-    $('.lbl-licitacao', html).each(function() {
+   $('span.col-sm-6', html).each(function() {
         console.log($(this).text());
       });
-    $('.col-sm-6 lbl-licitacao', html).each(function() {
-        console.log($(this).text());
+  
+    var Arr=[];
+    $('div.field-content table tr td:nth-child(2) ',html).each(function(index,value) {
+      
+        var link = $(value.attr('href'));
+        Arr.push({"link":link});
+        
       });
-      $('.col-sm-6 lbl-licitacao >a', html).each(function() {
-        console.log($(this).text());
-      });  
+      console.log(Arr);
     
   })
   .catch(function(err) {
